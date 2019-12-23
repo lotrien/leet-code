88eeab4448014d5cdf3da40d34af12be7aa6d42c
Olha Kurkaiedova <olya.kurkaedova@gmail.com>
2019-12-23 19:41:44 +0200

    Add daily solved tasks

---
+++ 88_mergeSortedArrays.js
@@ -0,0 +1,21 @@
+/**
+ * 88. Merge Sorted Array
+ */
+const merge = function(nums1, m, nums2, n) {
+  while (m > 0 && n > 0) {
+    if (nums1[m-1] >= nums2[n-1]) {
+      nums1[m+n-1] = nums1[m-1];
+      m--;
+    } else {
+      nums1[m+n-1] = nums2[n-1];
+      n--;
+    }
+  }
+
+  while (n > 0) {
+    nums1[n-1] = nums2[n-1];
+    n--;
+  }
+
+  return nums1;
+}
